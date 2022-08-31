d161b6399a5d177a813c50114b1e8d0ae267146a
Renan <orefusado@gmail.com>
2022-08-31 05:40:31 -0300

    terminado a estilização do card

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'inner-glow': 'inset 0 0 100px -60px #ffffff11;',
+        'innerx-glow': 'inset 140px 0 250px -300px #ffffff45, inset -140px 0 250px -300px #ffffff45',
+      }
+    },
   },
   plugins: [],
 }
